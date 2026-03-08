2844e6c7f4040110683f66d6439cf8b590e90e03
barcode-scammer <gfgdfgdfdgf73@gmail.com>
2026-03-08 19:26:51 +0800

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,25 +1,56 @@
-const scale = 3;
+/**
+ * Numbers of decimal digits to round to
+ */
+ const scale = 3;
 
+/**
+ * Calculate the score awarded when having a certain percentage on a list level
+ * @param {Number} rank Position on the list
+ * @param {Number} percent Percentage of completion
+ * @param {Number} minPercent Minimum percentage required
+ * @returns {Number}
+ */
 export function score(rank, percent, minPercent) {
-    // Levels above 46 are Legacy
-    if (rank > 46) return "Legacy";
-
-    if (rank > 150) return 0;
-    if (rank > 60 && percent < 100) return 0;
-
-    let baseScore =
-        (1500 - 33 * (rank - 1)) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    baseScore = Math.max(0, baseScore);
-
-    if (percent !== 100) {
-        return round(baseScore * (2 / 3));
+    if (rank > 150) {
+        return 0;
+    }
+    if (rank > 60 && percent < 100) {
+        return 0;
     }
 
-    return round(baseScore);
+    // Old formula
+    /*
+    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
+    // New formula
+let score =
+  (1500 - 33* (rank - 1)) *
+  ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
 }
 
 export function round(num) {
-    return Math.round(num * 10 ** scale) / 10 ** scale;
-}
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
+} 

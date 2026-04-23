9a8f6cacfa661280f4dbe3cbeabb372356372957
barcode-scammer <gfgdfgdfdgf73@gmail.com>
2026-04-23 18:42:00 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">Skillsets</div>
+                            <p>{{ level.skillsets || 'Unassigned' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>

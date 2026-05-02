3b8708e4c588cb28d80f40cb30237ee54c5f4c32
barcode-scammer <gfgdfgdfdgf73@gmail.com>
2026-05-03 00:16:52 +1000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
     */
     // New formula
  let score =
-  (1500 - 20* (rank - 1)) *
+  (1500 - 19* (rank - 1)) *
   ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
 

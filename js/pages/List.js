da25afbbe77791b203af8f81f07775ab0846ee22
barcode-scammer <gfgdfgdfdgf73@gmail.com>
2026-05-15 13:13:03 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -70,8 +70,8 @@ export default {
                             <td class="mobile">
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
-                            <td class="hz">
-                                <p>{{ record.hz }}fps</p>
+                            <td class="enj">
+                                <p>{{ record.enj }}/10</p>
                             </td>
                         </tr>
                     </table>
